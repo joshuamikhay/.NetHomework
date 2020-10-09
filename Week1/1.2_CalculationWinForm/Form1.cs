2248d9605ba00e603bf0a55fdbcabc5e3217e44c
2018326660145 Joshua Mikhay Kusuma <57247152+joshuamikhay@users.noreply.github.com>
2020-10-09 15:19:45 +0700

    Add files via upload

---
+++ Week1/1.2_CalculationWinForm/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._2_CalculationWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double result = 0.0;
+            string opCode ="";
+            listBox1.SelectedItem = opCode;
+            double firstValue = Convert.ToDouble(this.textBox1.Text);
+            double secondValue = Convert.ToDouble(this.textBox2.Text);
+            switch (opCode)
+            {
+                case "+":
+                    result = Convert.ToDouble( firstValue+ secondValue);
+                    textBox3.Text = Convert.ToString(result);
+                    break;
+                case "-":
+                    result = Convert.ToDouble(firstValue - secondValue);
+                    textBox3.Text = Convert.ToString(result);
+                    break;
+                case "*":
+                    result = Convert.ToDouble(firstValue * secondValue);
+                    textBox3.Text = Convert.ToString(result);
+                    break;
+                case "/":
+                    result = Convert.ToDouble(firstValue / secondValue);
+                    textBox3.Text = Convert.ToString(result);
+                    break;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Week1/1.2_CountingWinForm/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._2_CountingWinForm
+{
+    
+
+    public partial class Form1 : Form
+    {
+        double result = 0.0;
+        string opCode = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double countingOperation(double a, double b)
+        {
+            opCode = textBox4.Text;
+            switch(opCode)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+            }
+            return Convert.ToDouble(result);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double firstValue = Double.Parse(this.textBox1.Text);
+            double secondValue = Double.Parse(this.textBox2.Text);
+            countingOperation(firstValue, secondValue);
+            textBox3.Text = Convert.ToString(result);
+        }
+    }
+}
